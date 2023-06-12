506c8c700a45eb2e536d304105e9c9b3268cf31e
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-06-13 00:48:00 +0530

    added the data for the Sepolia testnet in helper.config file added the erros and Breeds of dogs in RandomIPFSNFT contract also set up the chainlink vrf coordinator

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -3,13 +3,43 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract RandomIpfs is VRFConsumerBaseV2{
-    //when we mint an NFT, we will get a random number 
+//stating the erros now
+error RandomIpfsNft__AlreadyInitialized();
+error RandomIpfsNft__NeedMoreETHSent();
+error RandomIpfsNft__RangeOutOfBounds();
+error RandomIpfsNft__TransferFailed();
+
+contract RandomIpfsNft is VRFConsumerBaseV2 {
+    //when we mint an NFT, we will get a random number
     //using that random number, we will get an random NFT
     //users have to pay to mint nft
     //the owner of the contract can withdraw the NFT
 
+    //Types
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+    //chainlink vrf variables
+    VRFCoordinatorV2Interface immutable private i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS=3;
+    uint32 private constant NUM_WORDS = 1;
+
+    //what eactly is this address vrfCoordinatorV2
+    constructor(address _vrfCoordinatorV2) VRFConsumerBaseV2(_vrfCoordinatorV2) 
+    {
+        i_vrfCoordinator = new VRFCoordinatorV2Interface(_vrfCoordinatorV2); //this is the vrf coordinator
+    }
     function requestNFT() public {}
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
-    fuction tokenURI(uint256) public {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {;}
+
+    function tokenURI(uint256) public {;}
 }
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -2,6 +2,11 @@
 interface networkConfigItem {
     name?: string;
     ethUsdPriceFeed?: string;
+    vrfCoordinatorV2?: string;
+    gasLane?: string;
+    callbackGasLimit?: string;
+    mintFee?: string;
+    subscriptionId?: string;
 }
 interface networkConfigInfo {
     [key: number]: networkConfigItem;
@@ -14,6 +19,12 @@ export const networkConfig: networkConfigInfo = {
     11155111: {
         name: "sepolia",
         ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625", //taken from chainlink itself
+        gasLane:
+            "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        callbackGasLimit: "500000", // 500,000 gas
+        mintFee: "10000000000000000", // 0.01 ETH
+        subscriptionId: "2678", // add your ID here!
     },
 };
 
